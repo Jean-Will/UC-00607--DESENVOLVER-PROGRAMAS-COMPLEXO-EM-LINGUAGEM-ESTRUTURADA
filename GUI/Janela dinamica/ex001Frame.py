62c513b2339d09f68ce8288ffb286a47d282e3f5
Jean-Will <jeanwill93@hotmail.com>
2026-09-23 12:59:32 +0100

    get dados to show

--- GUI/Janela dinamica/ex001Frame.py
+++ GUI/Janela dinamica/ex001Frame.py
@@ -8,6 +8,21 @@ def clock():
     timeLabel.after(1000,clock)
 
 
+def clear():
+    entryName.delete(0,END)
+    addressEntry.delete(0, END)
+    dateNameLabel.configure(text="")
+    dateAddressLabel.configure(text="")
+    entryName.focus()
+
+
+def read():
+    dateName = entryName.get()
+    dateAddress = addressEntry.get()
+    dateNameLabel.configure(text=f"O nome e: {dateName}")
+    dateAddressLabel.configure(text=f"A morada e: {dateAddress}")
+
+
 #############################################################
 
 windows = Tk()
@@ -29,10 +44,13 @@ f4 = Frame(windows)
 
 
 f1.pack(pady=10)
+
 nameLabel = Label(f1,text="Nome:")
 entryName = Entry(f1)
 nameLabel.grid(row=0, column=0)
 entryName.grid(row=0, column=1)
+entryName.focus() # arranca o programa ja na primeira caixa para preencher !!
+
 
 addressLabel = Label(f1,text="Morada")
 addressEntry = Entry(f1)
@@ -44,13 +62,14 @@ addressEntry.grid(row=1, column=1)
 
 
 f2.pack(pady=20)
-btn1Read = Button(f2, text="Ler Dados", width=10, )
+
+btn1Read = Button(f2, text="Ler Dados", width=10, command=read)
 btn1Read.grid( row=0,column=0,padx=3)
 btn1Read.configure(bd=3)
 
 
 
-btn2Clean = Button(f2, text="Limpar", width=10,)
+btn2Clean = Button(f2, text="Limpar", width=10, command=clear)
 btn2Clean.grid(row=0,column=1,padx=3)
 btn2Clean.configure(bd=3)
 
@@ -63,10 +82,13 @@ btn3Exit.configure(bd=3)
 
 f3.pack(pady=10)
 dateLabe = Label(f3, text="Os dados lidos sao:")
-dateLabe.grid(row=0, column=1)
+dateLabe.grid(row=0, column=0)
 
 dateNameLabel = Label(f3)
 dateAddressLabel = Label(f3)
+
+dateNameLabel.grid(row=1, column=0)
+dateAddressLabel.grid(row=2, column=0)
 
 ########################################################################################################################
 
